074be58a290f70cc326c25c615d736d8a04d529b
mahmou6 <mahmou6@gmail.com>
2018-04-10 17:47:32 +0200

    first commit after internet is ack

--- Window-shopper/Window-shopper/Controller/MainVC.swift
+++ Window-shopper/Window-shopper/Controller/MainVC.swift
@@ -9,15 +9,47 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+  
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let calBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calBtn.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        calBtn.setTitle("Calculate", for: .normal)
+        calBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calBtn
+        priceTxt.inputAccessoryView = calBtn
+        
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+    }
+    
+    @IBAction func clearCalculatorPressed(_ sender: Any) {
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+        wageTxt.text = ""
+        priceTxt.text = ""
+    }
+    
+
+    @objc func calculate() {
+        if let wageTxt = wageTxt.text , let priceTxt = priceTxt.text {
+            if let wage = Double(wageTxt), let price = Double(priceTxt) {
+                view.endEditing(true)
+                resultLbl.isHidden = false
+                hoursLbl.isHidden = false
+                
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
     }
 
 
--- Window-shopper/Window-shopper/Model/Wage.swift
+++ Window-shopper/Window-shopper/Model/Wage.swift
@@ -7,3 +7,9 @@
 //
 
 import Foundation
+
+class Wage {
+    class func  getHours(forWage wage: Double, andPrice price: Double) -> Int {
+        return Int(ceil(price/wage))
+    }
+}
